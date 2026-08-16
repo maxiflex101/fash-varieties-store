ff287e388dfcddb93cedf12cf511594692321b38
maxiflex101 <maxiflexconsult@gmail.com>
2026-08-17 00:28:39 +0100

    Create Product model with schema definition

---
+++ backend/models/Product.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+  name: {type:String, required:true},
+  price: {type:Number, required:true},
+  category: String,
+  description: String,
+  image: String,
+  stock: {type:Number, default:100}
+}, {timestamps:true});
+module.exports = mongoose.model('Product', productSchema);
